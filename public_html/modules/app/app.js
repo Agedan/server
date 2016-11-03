9491b62623617ed17f728065687dc112b677cfc5
Agedan <agedan17@gmail.com>
2016-11-03 17:05:13 +0300

    added private templates (still not working properly)

--- public_html/modules/app/app.js
+++ public_html/modules/app/app.js
@@ -1,13 +1,23 @@
 angular
-	.module('app', ['ui.router', 'chat'])
+	.module('app', ['ui.router', 'chat', 'auth'])
 	.config(function($stateProvider) {
+	  
+	 
 	  
 		$stateProvider.state('main',{
 			abstract : true,
 			views:{
 				'mainView':{
 					templateUrl: '/views/main.html'
-				},
+				}
+			}
+			
+		});
+		
+		
+		$stateProvider.state('main.private',{
+			abstract : true,
+			views:{
 				'chat@main':{
 					templateUrl: '/views/chat.html'
 				}
@@ -15,33 +25,49 @@ angular
 			
 		});
 		
-		$stateProvider.state('main.oldIndex',{
-			url: '/oldIndex',
+		
+		$stateProvider.state('main.login',{
+			url: '/login',
 			views:{
 				'content@main':{
-					templateUrl: '/views/old.html'
-				},
+					templateUrl: '/login.html'
+				}
 			}
-			
 		});
 
 		
-		$stateProvider.state('main.map',{
+		$stateProvider.state('main.private.map',{
 			url: '/map',
 			views:{
 				'content@main':{
 					templateUrl: '/views/map.html'
 				}
+			},
+			resolve : {
+				      isPrivate: ['$http', function($http) {
+						return $http({
+						  method: 'GET',
+						  url: '/views/map.html'
+						});
+					  }]
 			}
 			
 		});
 	  
-	  	$stateProvider.state('main.city',{
+	  	$stateProvider.state('main.private.city',{
 			url: '/city',
 			views:{
 				'content@main':{
 					templateUrl: '/views/city.html'
 				}
+			},
+			resolve : {
+				      isPrivate: ['$http', function($http) {
+						return $http({
+						  method: 'GET',
+						  url: '/views/city.html'
+						});
+					  }]
 			}
 			
 		});
@@ -52,8 +78,24 @@ angular
 		
 
 	})
-	//.config(["$locationProvider", function($locationProvider) {
-	//  $locationProvider.html5Mode(true);
-	//}]) раскоментировать после переноса на сервер и добавить base в head
-		.run(function($state) { $state.go('main.oldIndex');});
+	.config(["$locationProvider", function($locationProvider) {
+	 $locationProvider.html5Mode(true);
+	}]) 
+		.run(function($state) { 
+				      
+		$state.go('main.login');
+
+      //console.log(res);
+    });
+		
+
+
+   
+		
+
+
+		
+		
+		
+		
 	
---
+++ public_html/modules/app/auth.js
@@ -0,0 +1,73 @@
+﻿angular
+
+	.module('auth', [])
+	.config(function($httpProvider) {
+	  
+	  
+	  
+	  
+	  var interceptor = ['$q', '$injector', function($q, $injector) {
+return {
+    request: function(config) {
+      return config;
+    },
+
+    requestError: function(config) {
+      return config;
+    },
+
+    response: function(res) {
+	alert(res.status);
+      return res;
+
+    },
+
+    responseError: function(res) {
+	alert(res.status);
+	if(res.status === 401) {
+            $injector.get('$state').transitionTo('main.login');
+            return $q.reject(res);
+        }
+      return res;
+    }
+  }
+
+        
+
+}];
+
+$httpProvider.interceptors.push(interceptor);
+	  
+	  
+	  
+	  
+	  
+	  
+		
+
+	})
+	.controller('AuthController', function ($scope,$http) {
+
+	$scope.login = function (){
+	return $http({
+						  method: 'POST',
+						  url: '/api/v1/sessions',
+						  data: 'login='+$scope.userName+'&pass='+$scope.userPass
+						});
+		
+	}
+	  
+    });
+
+		
+
+
+   
+		
+
+
+		
+		
+		
+		
+	
